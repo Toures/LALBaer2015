7c2c2c1f81302d66d7a225b9a0a37254a0b10ec8
m.klug <m.klug@141.19.144.26>
2015-06-12 12:37:20 +0200

    Felder, getter und setter implementiert

--- src/de/hs_mannheim/IB/SS15/OOT/PlanObjects/Exam.java
+++ src/de/hs_mannheim/IB/SS15/OOT/PlanObjects/Exam.java
@@ -1,17 +1,62 @@
 package de.hs_mannheim.IB.SS15.OOT.PlanObjects;
 
+import de.hs_mannheim.IB.SS15.OOT.Subject;
+import de.hs_mannheim.IB.SS15.OOT.Participants.Assessor;
+import de.hs_mannheim.IB.SS15.OOT.Participants.Examinee;
+import de.hs_mannheim.IB.SS15.OOT.Participants.Examiner;
+
 public class Exam implements PlanObject{
 
-	int length;
+	private int length;
+	private Subject[] subjects;
+	private Examinee examinee;
+	private Examiner[] examiner;
+	private Assessor assessor;
 	
-	public Exam() {
-		// TODO Auto-generated constructor stub
+	
+	public Exam(Subject[] subjects, Examinee examinee, Examiner[] examiner, Assessor assessor, int length) {
+		this.subjects = subjects;
+		this.examinee = examinee;
+		this.examiner = examiner;
+		this.assessor = assessor;
+		this.length = length;
 	}
 
 	@Override
 	public int getLength() {
-		// TODO Auto-generated method stub
-		return length;
+		return this.length;
+	}
+
+	public Assessor getAssessor() {
+		return assessor;
+	}
+
+	public void setAssessor(Assessor assessor) {
+		this.assessor = assessor;
+	}
+
+	public Examinee getExaminee() {
+		return examinee;
+	}
+
+	public void setExaminee(Examinee examinee) {
+		this.examinee = examinee;
+	}
+
+	public Examiner[] getExaminer() {
+		return examiner;
+	}
+
+	public void setExaminer(Examiner[] examiner) {
+		this.examiner = examiner;
+	}
+
+	public Subject[] getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(Subject[] subjects) {
+		this.subjects = subjects;
 	}
 
 }
