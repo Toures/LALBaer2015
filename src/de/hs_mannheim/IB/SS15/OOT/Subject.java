f587aaa7dbdfb487fecc774c432172cd52db02ef
markio1234 <mark.a.beckmann@web.de>
2015-06-12 12:29:04 +0200

    + Subject

--- src/de/hs_mannheim/IB/SS15/OOT/Subject.java
+++ src/de/hs_mannheim/IB/SS15/OOT/Subject.java
@@ -3,7 +3,10 @@ package de.hs_mannheim.IB.SS15.OOT;
 public class Subject {
 
 	public Subject() {
-		// TODO Auto-generated constructor stub
+		int i = 0;
+		i++;
+		int a = 2;
+		int b = i+a;
 	}
 
 }
