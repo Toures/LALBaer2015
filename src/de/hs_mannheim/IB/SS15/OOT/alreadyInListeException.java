c9c3e58310d0e70b9ed8302523cfcef149464a1b
Mark <Mark@Mark-PC.localdomain>
2015-06-19 08:35:24 +0200

    neue Methode im Backend Exception dafür brauchen wir aber evtl net

---
+++ src/de/hs_mannheim/IB/SS15/OOT/alreadyInListeException.java
@@ -0,0 +1,14 @@
+package de.hs_mannheim.IB.SS15.OOT;
+
+public class alreadyInListeException extends Exception{
+
+	public alreadyInListeException() {
+		
+	}
+	
+	public alreadyInListeException(String s){
+		super(s);
+	}
+	
+	
+}
