04736f1df0183da590b7ba896d80a61552d139f5
joper0r <josip.g@live.de>
2015-06-19 14:43:50 +0200

    einzelne rechtschreibfehler behoben

---
+++ src/de/hs_mannheim/IB/SS15/OOT/AlreadyInListeException.java
@@ -0,0 +1,14 @@
+package de.hs_mannheim.IB.SS15.OOT;
+
+public class AlreadyInListeException extends Exception{
+
+	public AlreadyInListeException() {
+		
+	}
+	
+	public AlreadyInListeException(String s){
+		super(s);
+	}
+	
+	
+}
