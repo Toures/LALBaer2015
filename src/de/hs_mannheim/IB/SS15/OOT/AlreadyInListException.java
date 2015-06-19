b42b6a833fc6f951124165b49d2bfbda922d9963
joper0r <josip.g@live.de>
2015-06-19 14:46:41 +0200

    rechtschreibfehler etc

---
+++ src/de/hs_mannheim/IB/SS15/OOT/AlreadyInListException.java
@@ -0,0 +1,14 @@
+package de.hs_mannheim.IB.SS15.OOT;
+
+public class AlreadyInListException extends Exception{
+
+	public AlreadyInListException() {
+		
+	}
+	
+	public AlreadyInListException(String s){
+		super(s);
+	}
+	
+	
+}
