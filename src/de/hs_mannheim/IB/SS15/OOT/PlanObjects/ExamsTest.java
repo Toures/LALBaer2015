8aa34660a947044d02e199c62d9a3f022f53914f
markio1234 <mark.a.beckmann@web.de>
2015-06-23 13:02:15 +0200

    Exams Test erstellt Methoden bitte ändern

--- src/de/hs_mannheim/IB/SS15/OOT/PlanObjects/ExamsTest.java
+++ src/de/hs_mannheim/IB/SS15/OOT/PlanObjects/ExamsTest.java
@@ -14,16 +14,20 @@ import de.hs_mannheim.IB.SS15.OOT.Participants.Examinee;
 import de.hs_mannheim.IB.SS15.OOT.Participants.Examiner;
 
 public class ExamsTest {
-	private Exam exam;
+	private Exam exam, exam2, exam3;
 	private Subject[] subjects = new Subject[2];
+	private Subject[] subjects2 = new Subject[2];
+	private Subject[] subjects3 = new Subject[2];
 	private Examinee examinee;
 	private Examiner[] examiner = new Examiner[2];
 	private Assessor assessor;
+	Subject subjectOne; 
+	Subject subjectTwo ;
 	
 	@Before
 	public void setUp() throws Exception{
-		Subject subjectOne = new Subject("Lineare Algebra", "LAL");
-		Subject subjectTwo = new Subject("Analysis", "ANA");
+		subjectOne  = new Subject("Lineare Algebra", "LAL");
+		subjectTwo = new Subject("Analysis", "ANA");
 		subjects[0]= subjectOne;
 		subjects[1]=subjectTwo;
 		ArrayList<Subject> subjectsArrayList = new ArrayList<Subject>();
@@ -38,5 +42,33 @@ public class ExamsTest {
 		examiner[1]=examinerTestTwo;
 		Assessor assessorTest = new Assessor("Helene", subjectsArrayList);
 		exam = new Exam(subjects, examineeTest, examiner, assessorTest,3);
+		subjects3[0]=subjectOne;
+		exam2= new Exam(subjects2, examineeTest, examiner, assessorTest,3);
+		exam3= new Exam(subjects3, examineeTest, examiner, assessorTest,3);
+	}
+	
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void addSubjectTest_NameNull(){
+		exam.addSubject(null);
+		exam2.addSubject(null);
+		exam3.addSubject(null);
+	}
+	@Test// (expected = SameSubjectException.class)
+	public void addSubjectTest_ArrayHalfFullSameSubject(){
+		exam3.addSubject(subjectOne);
+		exam.addSubject(subjectOne);
+	}
+	
+	@Test
+	public void addSubjectTest_ArrayHalfFull_Legal(){
+		assertEquals(true, exam3.addSubject(subjectTwo));
+	}
+	
+	@Test
+	public void addSubjectTest_LegalArguments(){
+		assertEquals(true, exam2.addSubject(subjectOne));
+		assertEquals(true, exam2.addSubject(subjectTwo));
+		assertEquals(true, exam3.addSubject(subjectTwo));
 	}
 }
