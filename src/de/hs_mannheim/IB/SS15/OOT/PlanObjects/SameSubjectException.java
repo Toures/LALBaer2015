136b8b32bffbb0f87aff08f9b1a85ab40fe64d64
j.grabant <j.grabant@141.19.145.26>
2015-06-23 12:58:45 +0200

    SameSubjectException hinzugefügt

---
+++ src/de/hs_mannheim/IB/SS15/OOT/PlanObjects/SameSubjectException.java
@@ -0,0 +1,13 @@
+package de.hs_mannheim.IB.SS15.OOT.PlanObjects;
+
+public class SameSubjectException extends Exception {
+	
+	public SameSubjectException() {
+	
+	}
+	public SameSubjectException(String s){
+		super(s);
+	}
+	
+
+}
